7f4600dcc0ed4ddac6416e3e6f23997519aaf1d5
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-07-04 20:58:29 +0000

    Page WithAuth wrapper component

--- pages/drafts.js
+++ pages/drafts.js
@@ -2,27 +2,22 @@ import React from "react";
 import { request, gql } from "graphql-request";
 import Layout from "../components/Layout";
 import Post from "../components/Post";
-import { useSession } from "next-auth/react";
+import WithAuth from "../lib/WithAuth";
 
 export default function Drafts(props) {
-  const { _ } = useSession({
-    required: true,
-    onUnauthenticated() {
-      router.push("/api/auth/signin");
-    },
-  });
-
   return (
-    <Layout>
-      <h1>My Drafts</h1>
-      <div>
-        {props.drafts.map((post) => (
-          <div key={post.id} className="post">
-            <Post post={post} />
-          </div>
-        ))}
-      </div>
-    </Layout>
+    <WithAuth>
+      <Layout>
+        <h1>My Drafts</h1>
+        <div>
+          {props.drafts.map((post) => (
+            <div key={post.id} className="post">
+              <Post post={post} />
+            </div>
+          ))}
+        </div>
+      </Layout>
+    </WithAuth>
   );
 }
 
--- pages/profile.js
+++ pages/profile.js
@@ -1,40 +1,18 @@
-import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import Layout from "../components/Layout";
+import WithAuth from "../lib/WithAuth";
 
 export default function ProfilePage() {
-  const router = useRouter();
-  const { data: session, status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      router.push("/api/auth/signin");
-    },
-  });
-
-  if (status === "unauthenticated") {
-    return (
-      <Layout>
-        <div>
-          You need to be signed In. <h1>Redirecting to Sign In Page</h1>
-        </div>
-      </Layout>
-    );
-  }
-
-  if (status === "loading") {
-    return (
-      <Layout>
-        <div>Page Loading...</div>;
-      </Layout>
-    );
-  }
+  const { data: session, status } = useSession();
 
   return (
-    <Layout>
-      <div>
-        <h2>Profile Page</h2>
-        <pre>{JSON.stringify(session, null, 2)}</pre>
-      </div>
-    </Layout>
+    <WithAuth>
+      <Layout>
+        <div>
+          <h2>Profile Page</h2>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+        </div>
+      </Layout>
+    </WithAuth>
   );
 }
