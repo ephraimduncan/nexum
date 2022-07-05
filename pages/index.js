6819286e3d7df3bda4d1353147f1d451f73242a7
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-07-05 07:27:38 +0000

    view published posts on the main page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,51 @@
+import request, { gql } from "graphql-request";
 import { useSession } from "next-auth/react";
 import Layout from "../components/Layout";
+import Post from "../components/Post";
 
-export default function Home() {
+export default function Home(props) {
   const { data: session, status } = useSession();
 
   return (
     <Layout>
       {session ? (
         <div>
-          Logged In User
-          <pre>{JSON.stringify(session, null, 2)}</pre>
+          <h1>Posts</h1>
+          {props.publishedPosts.map((post) => {
+            return (
+              <div key={post.id} className="post">
+                <Post post={post} />
+              </div>
+            );
+          })}
         </div>
       ) : (
-        <div>Not Logged In</div>
+        <div>You need to be logged In.</div>
       )}
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req, res }) {
+  const query = gql`
+    {
+      publishedPosts {
+        content
+        id
+        published
+        title
+        author {
+          email
+          name
+          id
+        }
+      }
+    }
+  `;
+
+  const data = await request("http://localhost:3000/api/graphql", query);
+
+  return {
+    props: data,
+  };
 }
