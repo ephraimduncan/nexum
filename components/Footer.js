8fb4b56e9f754b28c754635292e522aa3191c08a
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-07-05 18:27:22 +0000

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://www.github.com/dephraiim/nexum" target="_blank" rel="noreferrer">
+        github.com/nexum
+      </a>
+    </footer>
+  );
+}
